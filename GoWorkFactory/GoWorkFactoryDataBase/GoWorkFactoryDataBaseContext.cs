e46353c2f3e34af53999a1adf16d3af9d9a52526
bertikq <bertikq@yandex.ru>
2020-04-30 14:55:30 +0400

    Start setting

---
+++ GoWorkFactory/GoWorkFactoryDataBase/GoWorkFactoryDataBaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoWorkFactoryDataBase
+{
+    public class GoWorkFactoryDataBaseContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql(@"Host=localhost;Port=5432;Database=GoWorkFactory;Username=postgres;Password=postgres");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
