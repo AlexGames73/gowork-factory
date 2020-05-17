e9bad17e5415adaf583060ebf0112f2b85b11c14
APGames <funnymanalex25@gmail.com>
2020-05-17 15:26:45 +0400

    base logic

---
+++ GoWorkFactory/GoWorkFactoryBusinessLogic/Interfaces/ILogic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoWorkFactoryBusinessLogic.Interfaces
+{
+    public interface ILogic<TBinding, TView>
+    {
+        void Create(TBinding model);
+        void Update(TBinding model);
+        void Remove(TBinding model);
+        IEnumerable<TView> Read(TBinding model);
+    }
+}
